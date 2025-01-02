4e9307599a268361283339d92fff603016865460
Jordan-Porter <jordanporter07@hotmail.co.uk>
2025-01-02 13:58:12 +0000

    added delta to game loop

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    time = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,8 @@ def main():
         pygame.Surface.fill(screen, (0,0,0))
 
         pygame.display.flip()
+
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
