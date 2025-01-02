a2922bf7a012d140595ab14dd90dc10a402791fb
Jordan-Porter <jordanporter07@hotmail.co.uk>
2025-01-02 14:45:59 +0000

    group update and draw calls

--- main.py
+++ main.py
@@ -14,21 +14,28 @@ def main():
     time = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        # update
+        for u in updatable:
+            u.update(dt)
+
+        # draw
         pygame.Surface.fill(screen, (0,0,0))
+        for d in drawable:
+            d.draw(screen)
 
-        player.draw(screen)
-
-        player.update(dt)
 
         pygame.display.flip()
-
         dt = time.tick(60) / 1000
 
 
